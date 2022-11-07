01f7f170374eb0b4f36ce7eb2900b8c0cd4261e5
mozafar <kabaros@gmail.com>
2022-11-07 14:55:07 +0000

    refactor: rename useResolvedDirection hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,3 @@
-export { useResolvedDir } from "./useResolvedDir";
+export { useResolvedDirection } from "./useResolvedDirection";
 export { useResolvedLocaleOptions } from "./useResolvedLocaleOptions";
 export { useDatePicker } from "./useDatePicker";
