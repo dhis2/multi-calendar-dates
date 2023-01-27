53641ec428a3febebaa4bdbf254c14c51d03ffba
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2023-01-27 10:45:10 +0800

    fix(build daily fixed period): add periodType to period

--- src/period-calculation/daily-periods/build-daily-fixed-period.ts
+++ src/period-calculation/daily-periods/build-daily-fixed-period.ts
@@ -1,13 +1,17 @@
 import { Temporal } from '@js-temporal/polyfill'
 import { formatYyyyMmDD } from '../../utils/helpers'
+import { FixedPeriod } from '../types'
 
-const buildDailyFixedPeriod = ({ date }: { date: Temporal.PlainDate }) => {
+type BuildDailyFixedPeriod = (args: { date: Temporal.PlainDate }) => FixedPeriod
+
+const buildDailyFixedPeriod: BuildDailyFixedPeriod = ({ date }) => {
     const year = date.year
     const nextDayMonthLabel = String(date.month).padStart(2, '0')
     const nextDayLabel = String(date.day).padStart(2, '0')
     const value = `${year}${nextDayMonthLabel}${nextDayLabel}`
 
     return {
+        periodType: 'DAILY',
         id: value,
         iso: value,
         name: formatYyyyMmDD(date),
