d6f8f3684572e0b023ee24757fed15453bb53fe8
Mozafar Haider <mozafar@dhis2.org>
2024-09-24 13:34:31 +0100

    refactor: remove unnecessary export before merging to main branch

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,4 @@ import './locales/index.js'
 export * from './hooks'
 export * as constants from './constants'
 export * from './period-calculation'
-export {
-    getNowInCalendar,
-    validateDateString,
-    convertFromIso8601,
-    convertToIso8601,
-} from './utils'
+export { getNowInCalendar, convertFromIso8601, convertToIso8601 } from './utils'
