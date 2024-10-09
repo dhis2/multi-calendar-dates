41c1736bf90d655ecb340ddabc41d6faaf19cfc0
Birk Johansson <birk@dhis2.org>
2024-10-09 17:56:55 +0200

    fix: use export type

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,3 @@
 export { useResolvedDirection } from './useResolvedDirection'
-export {
-    useDatePicker,
-    DatePickerOptions,
-    OnDateSelectPayload,
-} from './useDatePicker'
+export { useDatePicker } from './useDatePicker'
+export type { DatePickerOptions, OnDateSelectPayload } from './useDatePicker'
